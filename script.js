5cde187b17ea2a22e36c4f1d50deb6e5c569f84b
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-07-12 00:57:41 +0200

    Setting up Js file

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// Modal window
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelector('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  modal.classList.add('.hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.clientHeight; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', openModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
