29851470f42735aeddc9bd4497291e2542d14252
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-07-12 01:41:48 +0200

    Javascript will kill me

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
 
+///////////////////////////////////////
 // Modal window
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelector('.btn--show-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -19,7 +21,7 @@ const closeModal = function () {
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
