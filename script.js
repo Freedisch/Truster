b9363730596a1b1e0d46ee962c7ac90a98bfccde
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-22 15:07:02 +0200

    marker eventlistner

--- script.js
+++ script.js
@@ -10,3 +10,75 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapEvent;
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      const coords = [latitude, longitude];
+
+      map = L.map('map').setView(coords, 13);
+      //   var myIcon = L.icon({
+      //     iconUrl: 'my-icon.png',
+      //     iconSize: [38, 95],
+      //     iconAnchor: [22, 94],
+      //     popupAnchor: [0, 0],
+      //     shadowUrl: 'my-icon-shadow.png',
+      //     shadowSize: [68, 95],
+      //     shadowAnchor: [22, 94],
+      //   });
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      //   L.marker(coords)
+      //     .addTo(map)
+      //     .bindPopup('Your location.<br> Current position.')
+      //     .openPopup();
+
+      // Handling clicks on map event
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //Clear inputt field
+  inputDistance.value = inputDuration = inputCadence = inputElevation = '';
+
+  // Display marker
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 150,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
